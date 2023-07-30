dadd59766ec28af814ecc9b532adf0b420021258
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-29 19:51:05 -0500

    Basic nav bar

---
+++ app/tracks/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Tracks() {
+  return <div className="h-screen">Tracks</div>;
+}
+
+export default Tracks;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
