7d56ceba1f85b291ef72ec990582b6b7f8579f99
Justin Sixsmith <justinsixsmith@gmail.com>
2023-08-17 11:42:49 -0500

    Changes default text to black

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <DynamicHeaderWithNoSSR />
-        <main className="flex-grow text-[#EBEBEB]">{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
